a041a7bd558960f7e5f89c1e04854c134bd82ce4
Raja Biswas <rajjjabiswas@gmail.com>
2020-09-28 21:44:16 +0800

    initial app template

---
+++ index.py
@@ -0,0 +1,93 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+
+# must add this line in order for the app to be deployed successfully on Heroku
+from app import server
+from app import app
+
+
+dropdown = dbc.DropdownMenu(
+    children=[
+        dbc.DropdownMenuItem("Stats", href="/stats"),
+        dbc.DropdownMenuItem("Predictions", href="/lgbm-ml"),
+        dbc.DropdownMenuItem("FPL Squad", href="/squad"),
+    ],
+    nav = True,
+    in_navbar = True,
+    label = "Explore",
+)
+
+
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                # Use row and col to control vertical alignment of logo / brand
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(src="/assets/fpl-logo.jpg", height="30px")),
+                        dbc.Col(dbc.NavbarBrand("FPL Predictor", className="ml-2")),
+                    ],
+                    align="center",
+                    no_gutters=True,
+                ),
+                href="/home",
+            ),
+            dbc.NavbarToggler(id="navbar-toggler2"),
+            dbc.Collapse(
+                dbc.Nav(
+                    # right align dropdown menu with ml-auto className
+                    [dropdown], className="ml-auto", navbar=True
+                ),
+                id="navbar-collapse2",
+                navbar=True,
+            ),
+        ]
+    ),
+    color="dark",
+    dark=True,
+    className="mb-4",
+)
+
+
+def toggle_navbar_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+for i in [2]:
+    app.callback(
+        Output(f"navbar-collapse{i}", "is_open"),
+        [Input(f"navbar-toggler{i}", "n_clicks")],
+        [State(f"navbar-collapse{i}", "is_open")],
+    )(toggle_navbar_collapse)
+
+# embedding the navigation bar
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    navbar,
+    html.Div(id='page-content')
+])
+
+# embedding the navigation bar
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    navbar,
+    html.Div(id='page-content')
+])
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/lgbm-ml':
+        return html.Div("PWow P")
+    elif pathname == '/squad':
+        return html.Div("My Squad")
+    else:
+        return html.Div("This is Home")
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
